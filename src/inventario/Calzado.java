66ee4bc63a453cc58239e8ffcfee5f2057bdea91
Pierre Malaga <pierre.malaga.t@gmail.com>
2015-11-07 14:39:31 +0100

    Creación Repositorio

--- src/inventario/Calzado.java
+++ src/inventario/Calzado.java
@@ -1,12 +1,11 @@
 
 package inventario;
 
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Calzado extends Producto{
 
-    public Calzado(int codigo, String nombre, String descripcion, String talla, String marca, int stock, Double precio, enumTipo tipo, enumCategoria categoria, String sexo, LocalDateTime fecha) {
+    public Calzado(int codigo, String nombre, String descripcion, String talla, String marca, int stock, Double precio, enumTipo tipo, enumCategoria categoria, String sexo, LocalDate fecha) {
         super(codigo, nombre, descripcion, talla, marca, stock, precio, tipo, categoria, sexo, fecha);
     }
     
